a1020b68d419e36c3997a817eff6bde80ea49bd0
GlomGlom <apichate.yatra@gmail.com>
2020-02-11 12:14:02 +0700

    add new requirements

--- requirements.sh
+++ requirements.sh
@@ -8,24 +8,6 @@ wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 pip3 install opencv-python==4.1.0
 
-# install tensorflow
-
-CI_DOCKER_EXTRA_PARAMS="-e CI_BUILD_PYTHON=python3 -e CROSSTOOL_PYTHON_INCLUDE_PATH=/usr/include/python3.4" \
-    tensorflow/tools/ci_build/ci_build.sh PI-PYTHON3 \
-    tensorflow/tools/ci_build/pi/build_raspberry_pi.sh
-tensorflow/tools/ci_build/ci_build.sh PI \
-    tensorflow/tools/ci_build/pi/build_raspberry_pi.sh PI_ONE
-
-version=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
-
-if [ $version == "3.7" ]; then
-pip3 install tensorflow-2.0.0-cp37-none-linux_armv7l.whl
-fi
-
-if [ $version == "3.5" ]; then
-pip3 install tensorflow-1.14.0-cp35-none-linux_armv7l.whl
-fi
-
 # install keras
 
 sudo apt-get install python3-numpy
